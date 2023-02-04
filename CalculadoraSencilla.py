3530b03d6317cb9109727152c555efc25e6a0a0d
Nehuen <122373723+nehuenmartin@users.noreply.github.com>
2023-02-03 23:24:29 -0300

    Update CalculadoraSencilla.py
    se agrego bucle while

--- CalculadoraSencilla.py
+++ CalculadoraSencilla.py
@@ -8,22 +8,31 @@ def multiplicacion(x, y):
 def divicion(x, y):
     return x / y
 
-# Pregunta la operación a realizar
-operacion = input(f'selecciona la operación: \n a. Suma \n b. Resta \n c. Multiplicación \n d. División')
+while True:
+    try:
 
-# Pregunta los números a operar
-num_1 = int(input("Cual es el primer número: "))
-num_2 = int(input("Cual es el segundo número: "))
+        # Pregunta la operación a realizar
+        operacion = input(f'selecciona la operación: \n a. Suma \n b. Resta \n c. Multiplicación \n d. División')
 
-# Realiza la operación
-if operacion == 'a':
-    print(num_1,"+",num_2,"=", suma(num_1,num_2))
-elif operacion == 'b':
-    print(num_1,"-",num_2,"=", resta(num_1,num_2))
-elif operacion == 'c':
-    print(num_1,"*",num_2,"=", multiplicacion(num_1,num_2))
-elif operacion == 'd':
-    print(num_1,"/",num_2,"=", divicion(num_1,num_2))
-else:
-    print("Opción inválida")
+        # Pregunta los números a operar
+        num_1 = int(input("Cual es el primer número: "))
+        num_2 = int(input("Cual es el segundo número: "))
+
+        # Realiza la operación
+        if operacion == 'a':
+            print(num_1,"+",num_2,"=", suma(num_1,num_2))
+        elif operacion == 'b':
+            print(num_1,"-",num_2,"=", resta(num_1,num_2))
+        elif operacion == 'c':
+            print(num_1,"*",num_2,"=", multiplicacion(num_1,num_2))
+        elif operacion == 'd':
+            print(num_1,"/",num_2,"=", divicion(num_1,num_2))
+        repetir = input("¿Desea realizar otra operación? (s/n): ")
+        if repetir == 's':
+            continue
+        else:
+            break
+    except ValueError:
+        print("No se puede realizar la operación con letras")
+        continue
 
